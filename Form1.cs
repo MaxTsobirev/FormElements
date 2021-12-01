237da2d2f87a67958824c47a92160923a3d2eb2b
opilane <opilane@HP-CZC6297B8C>
2021-12-01 14:30:30 +0200

    listbox and menu

--- Form1.cs
+++ Form1.cs
@@ -24,6 +24,7 @@ namespace FormElements
         RadioButton rb;
         RadioButton rb2;
         TabControl tabC;
+        ListBox listBox1;
 
         public Form1()
         {
@@ -43,9 +44,11 @@ namespace FormElements
             tn.Nodes.Add(new TreeNode("Size"));
             tn.Nodes.Add(new TreeNode("Radio"));
             tn.Nodes.Add(new TreeNode("Radio2"));
-            tn.Nodes.Add(new TreeNode("Tekstkast-TextBox"));
             tn.Nodes.Add(new TreeNode("Kaart"));
             tn.Nodes.Add(new TreeNode("MessageBox"));
+            tn.Nodes.Add(new TreeNode("listBox1"));
+            tn.Nodes.Add(new TreeNode("DataGridView"));
+            tn.Nodes.Add(new TreeNode("MainMenu"));
 
             //button
             btn = new Button();
@@ -57,7 +60,7 @@ namespace FormElements
             //pealkiri
             lbl = new Label();
             lbl.Text = "Elementide liimite c# abil";
-            lbl.Size = new Size(150,20);
+            lbl.Size = new Size(150, 20);
             lbl.Location = new Point(120, 0);
             lbl.MouseHover += Lbl_MouseHover;
             lbl.MouseLeave += Lbl_MouseLeave;
@@ -76,7 +79,7 @@ namespace FormElements
             cb.Size = new Size(100, 20);
             cb.Location = new Point(200, 150);
             cb.Text = "Смена фона";
-            
+
             //cb2
             cb2 = new CheckBox();
             cb2.CheckedChanged += Cb2_CheckedChanged;
@@ -95,7 +98,19 @@ namespace FormElements
             rb2.Size = new Size(100, 20);
             rb2.Location = new Point(200, 300);
             rb2.Text = "Teema";
-            //tab
+            //listbox
+            listBox1 = new ListBox();
+            listBox1.Items.Add("green");
+            listBox1.Items.Add("yellow");
+            listBox1.Items.Add("blue");
+            listBox1.Items.Add("grey");
+            listBox1.Location = new Point(200, 250);
+            listBox1.Width = 100; listBox1.Height = 100;
+            listBox1.SelectedIndexChanged += ListBox1_SelectedIndexChanged;
+            //data
+            
+
+
 
 
 
@@ -104,193 +119,243 @@ namespace FormElements
             this.Controls.Add(tree);
         }
 
+        private void ListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch(listBox1.SelectedItem.ToString())
+            {
+                case ("blue"):tree.BackColor = Color.Blue;break;
+                case ("yellow"): tree.BackColor = Color.Yellow; break;
+                case ("green"): tree.BackColor = Color.Green; break;
+                case ("gray"): tree.BackColor = Color.Gray; break;
+            }
+        }
+
         private void Rb2_CheckedChanged(object sender, EventArgs e)
         {
-            
+
         }
 
-        private void Rb_CheckedChanged(object sender, EventArgs e)
-        {
-            lbl.Text = "1000-7?";
-        }
+            private void Rb_CheckedChanged(object sender, EventArgs e)
+            {
+                lbl.Text = "1000-7?";
+            }
 
-        private void Cb_CheckedChanged(object sender, EventArgs e)
-        {
-            if (cb.Checked)
+            private void Cb_CheckedChanged(object sender, EventArgs e)
             {
-                Image img = new Bitmap(@"C:\Users\opilane\source\repos\FormElements\g.jpg");
-                this.BackgroundImage = img;
-            }
-            else
-            {
-                Image img = new Bitmap(@"C:\Users\opilane\source\repos\FormElements\ti10.png");
-                this.BackgroundImage = img;
-            }
-            
-        }
-
-        private void Cb2_CheckedChanged(object sender, EventArgs e)
-        {
-            if (t)
-            {
-                this.Size = new Size(1200, 650);
-                cb2.Text = "Teeme väiksem";
-                t = false;
-            }
-            else
-            {
-                this.Size = new Size(760, 480);
-                cb2.Text = "Suurendame";
-                t = true;
-            }
-        }
-
-        private void Pic_DoubleClick(object sender, EventArgs e)
-        {
-            switch(count)
-            {
-                case 1:
-                    pic.Image = Image.FromFile(@"C:\Users\opilane\source\repos\FormElements\38b590e59031edd4cb9b2146a6.jpg");
-                    count++;
-                    break;
-                case 2:
-                    pic.Image = Image.FromFile(@"C:\Users\opilane\source\repos\FormElements\Horn_Toss_icon.png");
-                    count++;
-                    break;
-                case 3:
-                    pic.Image = Image.FromFile(@"C:\Users\opilane\source\repos\FormElements\d2teamspirit-728x420.png");
-                    count=1;
-                    break;
-                    
+                if (cb.Checked)
+                {
+                    Image img = new Bitmap(@"C:\Users\opilane\source\repos\FormElements\g.jpg");
+                    this.BackgroundImage = img;
+                }
+                else
+                {
+                    Image img = new Bitmap(@"C:\Users\opilane\source\repos\FormElements\ti10.png");
+                    this.BackgroundImage = img;
+                }
 
             }
 
-            
-            
-            
+            private void Cb2_CheckedChanged(object sender, EventArgs e)
+            {
+                if (t)
+                {
+                    this.Size = new Size(1200, 650);
+                    cb2.Text = "Teeme väiksem";
+                    t = false;
+                }
+                else
+                {
+                    this.Size = new Size(760, 480);
+                    cb2.Text = "Suurendame";
+                    t = true;
+                }
+            }
+
+            private void Pic_DoubleClick(object sender, EventArgs e)
+            {
+                switch (count)
+                {
+                    case 1:
+                        pic.Image = Image.FromFile(@"C:\Users\opilane\source\repos\FormElements\38b590e59031edd4cb9b2146a6.jpg");
+                        count++;
+                        break;
+                    case 2:
+                        pic.Image = Image.FromFile(@"C:\Users\opilane\source\repos\FormElements\Horn_Toss_icon.png");
+                        count++;
+                        break;
+                    case 3:
+                        pic.Image = Image.FromFile(@"C:\Users\opilane\source\repos\FormElements\d2teamspirit-728x420.png");
+                        count = 1;
+                        break;
+
+
+                }
 
 
 
-        }
 
-        private void Lbl_MouseLeave(object sender, EventArgs e)
+
+
+
+            }
+
+            private void Lbl_MouseLeave(object sender, EventArgs e)
+            {
+                lbl.BackColor = Color.Transparent;
+            }
+
+            private void Lbl_MouseHover(object sender, EventArgs e)
+            {
+                lbl.BackColor = Color.FromArgb(200, 20, 30);
+            }
+
+            private void Btn_Click(object sender, EventArgs e)
+            {
+                this.Close();
+            }
+
+            private void Tree_AfterSelect(object sender, TreeViewEventArgs e)
+
+            {
+                if (e.Node.Text == "Nupp")
+                {
+                    this.Controls.Add(btn);
+                }
+                else if (e.Node.Text == "Silt")
+                {
+                    this.Controls.Add(lbl);
+                }
+                else if (e.Node.Text == "PictureBox")
+                {
+                    this.Controls.Add(pic);
+                }
+                else if (e.Node.Text == "Wallpaper")
+                {
+                    this.Controls.Add(cb);
+                }
+                else if (e.Node.Text == "Size")
+                {
+                    this.Controls.Add(cb2);
+                }
+                else if (e.Node.Text == "Radio")
+                {
+                    this.Controls.Add(rb);
+                }
+                else if (e.Node.Text == "Radio2")
+                {
+                    this.Controls.Add(rb2);
+                }
+                else if (e.Node.Text == "listBox1")
+                {
+                    this.Controls.Add(listBox1);
+                }
+                else if (e.Node.Text == "MessageBox")
+                {
+                    MessageBox.Show("MessageBox", "Kõige lihtsam aken");
+                    var answer = MessageBox.Show("Хочешь написать послание в будущее?", "Aken koos nupudega", MessageBoxButtons.YesNo);
+                    if (answer == DialogResult.Yes)
+                    {
+                        string text = Interaction.InputBox("Расскажи что нибудь", "InputBox", "Жили были ....");
+                        if (MessageBox.Show("Оно отправиться в будущее", "Teksti salvestamine", MessageBoxButtons.OKCancel) == DialogResult.OK)
+                        {
+                            lbl.Text = text;
+                            Controls.Add(lbl);
+                        }
+                        else
+                        {
+                            lbl.Text = "Siis mina lisan oma teksti!";
+                            Controls.Add(lbl);
+                        }
+                    }
+                }
+                else if (e.Node.Text == "Radio2")
+                {
+                    rb.CheckedChanged += new EventHandler(Rb_CheckedChanged);
+                    rb2.CheckedChanged += Rb2_CheckedChanged1;
+                }
+                else if (e.Node.Text == "Kaart")
+                {
+                    tabC = new TabControl();
+                    tabC.Location = new Point(150, 150);
+                    tabC.Size = new Size(300, 200);
+                    TabPage tabP1 = new TabPage("youtube");
+                    WebBrowser wb = new WebBrowser();
+                    wb.Url = new Uri("https://www.youtube.com");
+                    tabP1.Controls.Add(wb);
+                    TabPage tabP2 = new TabPage("qwqw");
+                    TabPage tabP3 = new TabPage("erer");
+                    tabP3.DoubleClick += TabP3_DoubleClick;
+                    tabC.Controls.Add(tabP1);
+                    tabC.Controls.Add(tabP2);
+                    tabC.Controls.Add(tabP3);
+                    tabC.Selected += TabC_Selected;
+                    this.Controls.Add(tabC);
+                }
+                else if (e.Node.Text == "DataGridView")
+                {
+                    DataSet ds = new DataSet("XML fail. Menüü");
+                    ds.ReadXml(@"..\..\XMLFile1.xml");
+                    DataGridView dg = new DataGridView();
+                    dg.Width = 400;
+                    dg.Height = 300;
+                    dg.Location = new Point(150, 250);
+                    dg.AutoGenerateColumns = true;
+                    dg.DataSource = ds;
+                    dg.DataMember = "Food";
+                    this.Controls.Add(dg);
+                }
+                else if (e.Node.Text=="MainMenu")
+                {
+                    MainMenu menu = new MainMenu();
+                    MenuItem menuFile = new MenuItem("File");
+                    menuFile.MenuItems.Add("Exit",new EventHandler(menuFile_Exit_Select));
+                    menu.MenuItems.Add(menuFile);
+                    this.Menu = menu;
+                    MenuItem About = menu.MenuItems.Add("&Click");
+                    About.MenuItems.Add(new MenuItem("&Click", new EventHandler(this.About_clicked), Shortcut.F1));
+                    this.Menu = menu;
+                }
+            }
+
+
+        private void About_clicked(object sender, EventArgs e)
         {
-            lbl.BackColor = Color.Transparent;
+            MessageBox.Show("Hello! ;)");
         }
-
-        private void Lbl_MouseHover(object sender, EventArgs e)
-        {
-            lbl.BackColor = Color.FromArgb(200, 20, 30);
-        }
-
-        private void Btn_Click(object sender, EventArgs e)
+        private void menuFile_Exit_Select(object sender, EventArgs e)
         {
             this.Close();
         }
+        private void TabC_Selected(object sender, TabControlEventArgs e)
+            {
+                this.tabC.TabPages.Clear();
+            }
 
-        private void Tree_AfterSelect(object sender, TreeViewEventArgs e)
-        {
-            if (e.Node.Text=="Nupp")
+            private void TabP3_DoubleClick(object sender, EventArgs e)
             {
-                this.Controls.Add(btn);
+                string title = "tabP" + (tabC.TabCount + 1).ToString();
+                TabPage tb = new TabPage(title);
+                tabC.TabPages.Add(tb);
             }
-            else if (e.Node.Text=="Silt")
+
+            private void Rb2_CheckedChanged1(object sender, EventArgs e)
             {
-                this.Controls.Add(lbl);
-            }
-            else if (e.Node.Text== "PictureBox")
-            {
-                this.Controls.Add(pic);
-            }
-            else if (e.Node.Text == "Wallpaper")
-            {
-                this.Controls.Add(cb);
-            }
-            else if (e.Node.Text == "Size")
-            {
-                this.Controls.Add(cb2);
-            }
-            else if (e.Node.Text == "Radio")
-            {
-                this.Controls.Add(rb);
-            }
-            else if (e.Node.Text == "Radio2")
-            {
-                this.Controls.Add(rb2);
-            }
-            else if(e.Node.Text=="MessageBox")
-            {
-                MessageBox.Show("MessageBox", "Kõige lihtsam aken");
-                var answer = MessageBox.Show("Хочешь написать послание в будущее?", "Aken koos nupudega",MessageBoxButtons.YesNo);
-                if(answer == DialogResult.Yes)
+                if (rb2.Checked)
                 {
-                    string text = Interaction.InputBox("Расскажи что нибудь", "InputBox", "Жили были ....");
-                    if(MessageBox.Show("Оно отправиться в будущее","Teksti salvestamine",MessageBoxButtons.OKCancel)==DialogResult.OK)
-                    {
-                        lbl.Text = text;
-                        Controls.Add(lbl);
-                    }
-                    else
-                    {
-                        lbl.Text = "Siis mina lisan oma teksti!";
-                        Controls.Add(lbl);
-                    }
+                    this.BackColor = Color.Black;
+                    rb2.ForeColor = Color.White;
+                    rb.ForeColor = Color.White;
+
+                }
+                else if (rb2.Checked)
+                {
+                    this.BackColor = Color.White;
+                    rb2.ForeColor = Color.Black;
+                    rb.ForeColor = Color.Black;
                 }
             }
-            else if (e.Node.Text == "Radio2")
-            {
-                rb.CheckedChanged += new EventHandler(Rb_CheckedChanged);
-                rb2.CheckedChanged += Rb2_CheckedChanged1;
-            }
-            else if (e.Node.Text=="Kaart")
-            {
-                tabC = new TabControl();
-                tabC.Location = new Point(150,150);
-                tabC.Size = new Size(300, 200);
-                TabPage tabP1 = new TabPage("youtube");
-                WebBrowser wb = new WebBrowser();
-                wb.Url =new Uri ("https://www.youtube.com");
-                tabP1.Controls.Add(wb);
-                TabPage tabP2 = new TabPage("qwqw");
-                TabPage tabP3 = new TabPage("erer");
-                tabP3.DoubleClick += TabP3_DoubleClick;
-                tabC.Controls.Add(tabP1);
-                tabC.Controls.Add(tabP2);
-                tabC.Controls.Add(tabP3);
-                tabC.Selected += TabC_Selected;
-                this.Controls.Add(tabC);
-            }
-        }
-
-        private void TabC_Selected(object sender, TabControlEventArgs e)
-        {
-            this.tabC.TabPages.Clear();
-        }
-
-        private void TabP3_DoubleClick(object sender, EventArgs e)
-        {
-            string title = "tabP" + (tabC.TabCount + 1).ToString();
-            TabPage tb = new TabPage(title);
-            tabC.TabPages.Add(tb);
-        }
-
-        private void Rb2_CheckedChanged1(object sender, EventArgs e)
-        {
-            if(rb2.Checked)
-            {
-                this.BackColor = Color.Black;
-                rb2.ForeColor = Color.White;
-                rb.ForeColor = Color.White;
-
-            }
-            else if (rb2.Checked)
-            {
-                this.BackColor = Color.White;
-                rb2.ForeColor = Color.Black;
-                rb.ForeColor = Color.Black;
-            }
-        }
+        
+            
+        
     }
-
-
 }
